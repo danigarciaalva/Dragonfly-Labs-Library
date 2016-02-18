6ac8d12bd85efd56a4576c717f662ae0429cd4b9
Daniel García Alvarado <danigarciaalva@hotmail.com>
2016-02-18 00:46:28 -0600

    Validators fixed

--- dflabslib/src/main/java/io/dflabs/lib/validators/Regex.java
+++ dflabslib/src/main/java/io/dflabs/lib/validators/Regex.java
@@ -7,7 +7,7 @@ package io.dflabs.lib.validators;
 @SuppressWarnings("unused")
 public class Regex {
 
-    private static final String EMAIL =
+    public static final String EMAIL =
             "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
                     + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
     public static final String ALPHA_NUMERIC =
